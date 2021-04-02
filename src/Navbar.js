88644ddb0dcea3f861a9a4272c568ec3cbf25952
Dan Mugisho <danmmwx@gmail.com>
2021-04-02 22:55:18 +0300

    Add the initial navbar

---
+++ src/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import "./Navbar.css"
+import {BrowserRouter as Router, Link, NavLink} from "react-router-dom";
+import { menuData } from "./menuData";
+import { MenuFoldOutlined } from "@ant-design/icons"
+import FollowBtn from "./FollowBtn";
+
+
+const Navbar = () => {
+
+    return (
+        <nav className="nav">
+            <div className="logo">
+                <Router>
+                    <Link to="#" className="navLink" >
+                        Logo
+                    </Link>
+                </Router>
+            </div>
+
+            <MenuFoldOutlined className="menu__bars" style={{ fontSize: '30px' }} />
+
+            <div className="navMenu">
+                {menuData.map((item, index) => (
+                    <Router>
+                        <Link to={item.link} key={index} className="navLink" >
+                            {item.title}
+                        </Link>
+                    </Router>
+                ))}
+            </div>
+
+            <div className="navBtn" >
+                {/*<FollowBtn />*/}
+            </div>
+
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/menuData.js
@@ -0,0 +1,22 @@
+export const menuData = [
+    {
+        title: 'Home',
+        link: '/home'
+    },
+    {
+        title: 'About',
+        link: '/about'
+    },
+    {
+        title: 'Band',
+        link: '/band'
+    },
+    {
+        title: 'Lyrics',
+        link: '/lyrics'
+    },
+    {
+        title: 'Gallery',
+        link: '/gallery'
+    }
+]
