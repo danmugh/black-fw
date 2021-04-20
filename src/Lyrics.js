58b163b6854e8dbf0e89f503025f8a0df80ed20a
Dan Mugisho <danmmwx@gmail.com>
2021-04-20 13:05:34 +0300

    Updating lyrics's page

--- src/Lyrics.js
+++ src/Lyrics.js
@@ -1,11 +1,27 @@
 import React, { useState } from 'react';
-import { Modal, Button } from 'antd';
+// import { Modal, Button } from 'antd';
 import "./Lyrics.css";
-import img from "./assets/testimony__3.jpeg";
-import { FireOutlined } from "@ant-design/icons";
+import { lyricsData } from "./LyricsData";
+
+// import img from "./assets/testimony__3.jpeg";
+// import { FireOutlined } from "@ant-design/icons";
 
 const Lyrics = () => {
-    const [visible, setVisible] = useState(false);
+    // const [visible, setVisible] = useState(false);
+
+    let indexPlus;
+
+    const [active, setActive] = useState(0);
+
+    const eventHandler = (e, index) => {
+        e.preventDefault();
+        setActive(index);
+    }
+
+    const indexCount = (index) => {
+        indexPlus = index + 1;
+        return indexPlus;
+    }
 
     return (
         <div className="Lyrics__container" >
@@ -13,131 +29,161 @@ const Lyrics = () => {
                 <h3>ALL LYRICS</h3>
                 <p >Lorem ipsum we'll play you some music.Remember to book copy your lyrics!</p>
 
-                <Button type="primary" onClick={() => setVisible(true)}>
-                    Open Modal of 1000px width
-                </Button>
+                <div className="Lyrics__accordion" >
+                    <form>
+                        { lyricsData.map((tab, index) => (
+                            <div key={index}>
+                                <h4>
+                                    <div className="a-wrapper" >
+                                        <a
+                                            onClick={(e) => eventHandler(e, index)}
+                                            className={ active === index ? 'active' : 'inactive'}
+                                            aria-expanded={ active === index ? 'true' : 'false' }
+                                            aria-controls={ 'sect-' + indexCount(index) }
+                                            aria-disabled={ active === index ? 'true' : 'false' }
+                                            tabIndex={indexCount(index)}
+                                        >
+                                            <span className="title-wrapper" >{tab.title}
+                                                <span className={ active === index  ? 'plus' : 'minus'}></span>
+                                            </span>
+                                        </a>
+                                    </div>
+                                </h4>
+                                <div className="description-wrapper" >
+                                    <div id={ 'sect-' + indexCount(index) } className={ active === index  ? 'panel-open' : 'panel-close' }>
+                                        <p>{ tab.description }</p>
+                                        {/*{ tab.description }*/}
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                        }
+                    </form>
+                </div>
+
+                {/*<Button type="primary" onClick={() => setVisible(true)}>*/}
+                {/*    Open Modal of 1000px width*/}
+                {/*</Button>*/}
 
 
             </div>
 
-            {/*<Button type="primary" onClick={() => setVisible(true)}>*/}
-            {/*    Open Modal of 1000px width*/}
-            {/*</Button>*/}
+
             {/*Modal*/}
-            <div>
-                <Modal
-                    // closeIcon={
-                    //     <FireOutlined />
-                    // }
-                    // mask={false}
-                    // bodyStyle="body__style"
+            {/*<div>*/}
+            {/*    <Modal*/}
+            {/*        // closeIcon={*/}
+            {/*        //     <FireOutlined />*/}
+            {/*        // }*/}
+            {/*        // mask={false}*/}
+            {/*        // bodyStyle="body__style"*/}
 
-                    // wrapClassName="modal__style"
+            {/*        // wrapClassName="modal__style"*/}
 
-                    bodyStyle={{ color: 'gray' }}
+            {/*        bodyStyle={{ color: 'gray' }}*/}
 
-                    title="Modal 1000px width"
-                    centered
-                    visible={visible}
-                    onCancel={() => setVisible(false)}
+            {/*        title="Modal 1000px width"*/}
+            {/*        centered*/}
+            {/*        visible={visible}*/}
+            {/*        onCancel={() => setVisible(false)}*/}
 
-                    // height={500}
-                    // width={700}
+            {/*        // height={500}*/}
+            {/*        // width={700}*/}
 
-                    footer={[
-                        <Button
-                            key="back"
-                            // onClick={this.handleCancel}
-                        >
-                            Return
-                        </Button>,
-                        <Button
-                            key="submit"
-                            type="primary"
-                            // loading={loading}
-                            // onClick={this.handleOk}
-                        >
-                            Submit
-                        </Button>,
-                        <Button
-                            key="link"
-                            href="https://google.com"
-                            type="primary"
-                            // loading={loading}
-                            // onClick={this.handleOk}
-                        >
-                            Search on Google
-                        </Button>,
-                    ]}
-                >
-                    <p>[Chorus]</p>
-                    <p>Do some people talk about us
-                        And I know it’s so struggle
-                        Some people talk about us
-                        I just wanna go back
-                        Lemme be the better one
-                        I just wanna hold ya !!!
-                        Do some people talk?
-                        Do they talk?
-                        Some people talk about us
-                        I just wanna go back
-                        Lemme be the better one
-                        I just wanna hold ya !!!</p>
-                    <p>[Verse 1]</p>
-                    <p>They always talk about me
-                        Always talk about you
-                        You know what I mean
-                        You know what I say
-                        You know what I’m talking to you
-                        Baby girl you be my bestie
-                        You be my all my honey
-                        I just wanna go back for you
-                        You be my all my angel
-                        Trynna to raise our relationship baby girl
-                        This wanna make reality
-                        My baby well
-                        You know what am talkin' to you
-                        Baby you know well
-                    </p>
-                    <p>[Chorus]</p>
-                    <p>Do some people talk about us
-                        And I know it’s so struggle
-                        Some people talk about us
-                        I just wanna go back
-                        Lemme be the better one
-                        I just wanna hold ya !!!
-                        Do some people talk?
-                        Do they talk?
-                        Some people talk about us
-                        I just wanna go back
-                        Lemme be the better one
-                        I just wanna hold ya !!!</p>
-                    <p>[Verse 2]</p>
-                    <p>You are doin' well
-                        You are feelin' it
-                        You are mad in it
-                        Fillin' up the stage
-                        Baby you know well
-                        Got somethin' juice too much sauce too much juice
-                        Dreams come true
-                        Mama told me that I gonna rise
-                        Fillin' up
-                        You deserve
-                        You do love me
-                        Fallen for you
-                        Love me
-                        Love you
-                        Baby girl you know well
-                        Talkin' to you
-                        Black FW S.K.U.D.A</p>
-                    <p>[Outro]</p>
-                    <p>Yeah
-                        I use to think about my haters
-                        Love is something you gotta work at
-                        Understand and overstanding
-                        One touch Images</p>
-                </Modal>
-            </div>
+            {/*        footer={[*/}
+            {/*            <Button*/}
+            {/*                key="back"*/}
+            {/*                // onClick={this.handleCancel}*/}
+            {/*            >*/}
+            {/*                Return*/}
+            {/*            </Button>,*/}
+            {/*            <Button*/}
+            {/*                key="submit"*/}
+            {/*                type="primary"*/}
+            {/*                // loading={loading}*/}
+            {/*                // onClick={this.handleOk}*/}
+            {/*            >*/}
+            {/*                Submit*/}
+            {/*            </Button>,*/}
+            {/*            <Button*/}
+            {/*                key="link"*/}
+            {/*                href="https://google.com"*/}
+            {/*                type="primary"*/}
+            {/*                // loading={loading}*/}
+            {/*                // onClick={this.handleOk}*/}
+            {/*            >*/}
+            {/*                Search on Google*/}
+            {/*            </Button>,*/}
+            {/*        ]}*/}
+            {/*    >*/}
+            {/*        <p>[Chorus]</p>*/}
+            {/*        <p>Do some people talk about us*/}
+            {/*            And I know it’s so struggle*/}
+            {/*            Some people talk about us*/}
+            {/*            I just wanna go back*/}
+            {/*            Lemme be the better one*/}
+            {/*            I just wanna hold ya !!!*/}
+            {/*            Do some people talk?*/}
+            {/*            Do they talk?*/}
+            {/*            Some people talk about us*/}
+            {/*            I just wanna go back*/}
+            {/*            Lemme be the better one*/}
+            {/*            I just wanna hold ya !!!</p>*/}
+            {/*        <p>[Verse 1]</p>*/}
+            {/*        <p>They always talk about me*/}
+            {/*            Always talk about you*/}
+            {/*            You know what I mean*/}
+            {/*            You know what I say*/}
+            {/*            You know what I’m talking to you*/}
+            {/*            Baby girl you be my bestie*/}
+            {/*            You be my all my honey*/}
+            {/*            I just wanna go back for you*/}
+            {/*            You be my all my angel*/}
+            {/*            Trynna to raise our relationship baby girl*/}
+            {/*            This wanna make reality*/}
+            {/*            My baby well*/}
+            {/*            You know what am talkin' to you*/}
+            {/*            Baby you know well*/}
+            {/*        </p>*/}
+            {/*        <p>[Chorus]</p>*/}
+            {/*        <p>Do some people talk about us*/}
+            {/*            And I know it’s so struggle*/}
+            {/*            Some people talk about us*/}
+            {/*            I just wanna go back*/}
+            {/*            Lemme be the better one*/}
+            {/*            I just wanna hold ya !!!*/}
+            {/*            Do some people talk?*/}
+            {/*            Do they talk?*/}
+            {/*            Some people talk about us*/}
+            {/*            I just wanna go back*/}
+            {/*            Lemme be the better one*/}
+            {/*            I just wanna hold ya !!!</p>*/}
+            {/*        <p>[Verse 2]</p>*/}
+            {/*        <p>You are doin' well*/}
+            {/*            You are feelin' it*/}
+            {/*            You are mad in it*/}
+            {/*            Fillin' up the stage*/}
+            {/*            Baby you know well*/}
+            {/*            Got somethin' juice too much sauce too much juice*/}
+            {/*            Dreams come true*/}
+            {/*            Mama told me that I gonna rise*/}
+            {/*            Fillin' up*/}
+            {/*            You deserve*/}
+            {/*            You do love me*/}
+            {/*            Fallen for you*/}
+            {/*            Love me*/}
+            {/*            Love you*/}
+            {/*            Baby girl you know well*/}
+            {/*            Talkin' to you*/}
+            {/*            Black FW S.K.U.D.A</p>*/}
+            {/*        <p>[Outro]</p>*/}
+            {/*        <p>Yeah*/}
+            {/*            I use to think about my haters*/}
+            {/*            Love is something you gotta work at*/}
+            {/*            Understand and overstanding*/}
+            {/*            One touch Images</p>*/}
+            {/*    </Modal>*/}
+            {/*</div>*/}
         </div>
 
     );
---
+++ src/LyricsData.js
@@ -0,0 +1,218 @@
+export const lyricsData = [
+    {
+        title: 'TALK T.I.N.D',
+        description: (
+            <div>
+                <p>[Chorus]</p>
+                <p>Do some people talk about us
+                    And I know it’s so struggle
+                    Some people talk about us
+                    I just wanna go back
+                    Lemme be the better one
+                    I just wanna hold ya !!!
+                    Do some people talk?
+                    Do they talk?
+                    Some people talk about us
+                    I just wanna go back
+                    Lemme be the better one
+                    I just wanna hold ya !!!</p>
+                <p>[Verse 1]</p>
+                <p>They always talk about me
+                    Always talk about you
+                    You know what I mean
+                    You know what I say
+                    You know what I’m talking to you
+                    Baby girl you be my bestie
+                    You be my all my honey
+                    I just wanna go back for you
+                    You be my all my angel
+                    Trynna to raise our relationship baby girl
+                    This wanna make reality
+                    My baby well
+                    You know what am talkin' to you
+                    Baby you know well
+                </p>
+                <p>[Chorus]</p>
+                <p>Do some people talk about us
+                    And I know it’s so struggle
+                    Some people talk about us
+                    I just wanna go back
+                    Lemme be the better one
+                    I just wanna hold ya !!!
+                    Do some people talk?
+                    Do they talk?
+                    Some people talk about us
+                    I just wanna go back
+                    Lemme be the better one
+                    I just wanna hold ya !!!</p>
+                <p>[Verse 2]</p>
+                <p>You are doin' well
+                    You are feelin' it
+                    You are mad in it
+                    Fillin' up the stage
+                    Baby you know well
+                    Got somethin' juice too much sauce too much juice
+                    Dreams come true
+                    Mama told me that I gonna rise
+                    Fillin' up
+                    You deserve
+                    You do love me
+                    Fallen for you
+                    Love me
+                    Love you
+                    Baby girl you know well
+                    Talkin' to you
+                    Black FW S.K.U.D.A</p>
+                <p>[Outro]</p>
+                <p>Yeah
+                    I use to think about my haters
+                    Love is something you gotta work at
+                    Understand and overstanding
+                    One touch Images</p>
+            </div>
+        )
+    },
+    {
+        title: 'GRTFL',
+        description: (
+            <div>
+                <p>[Intro]</p>
+                <p>I just wanna thank every body that left us,
+                    Just to make dem know that am grateful for what they did
+                    Specially my mom
+                    (Skuda)
+                    (Brush)</p>
+                <p>[Chorus]</p>
+                <p>Mama told me
+                    I got a plan for you
+                    Got somethin' gravy for your life
+                    You should know oh oh !!!
+                    Feel in it
+                    You know you got a plan
+                    Just hope in Jesus
+                    Baby boy
+                    You gonna rise </p>
+                <p>[Verse 1]</p>
+                <p>Always thinkin'
+                    I was goin' school
+                    Scared for my pains
+                    Always praise the Lord
+                    Daddy told me
+                    Study for your future
+                    Writin' many sounds
+                    Drinkin' some juice
+                    Back at home
+                    Tryin' to show the stem
+                    Winnin' for the same
+                    Waitin' for the food
+                    Gettin' more
+                    NK was my style
+                    I was nothin' li' ah ah ah
+                    Yeah eh eh eh
+                    (Skuda aanh)
+                </p>
+                <p>[Pre-chorus]</p>
+                <p>Everytime when I keep my face on it
+                    I got a plan to move million grounds
+                    Thinkin' 'bout my mama everyday ,
+                    In my life is you </p>
+                <p>[Chorus]</p>
+                <p>Mama told me
+                    I got a plan for you
+                    Got somethin' gravy for your life
+                    You should know oh oh !!!
+                    Feel in it
+                    You know you got a plan
+                    Just hope in Jesus
+                    Baby boy
+                    You gonna rise </p>
+                <p><em>SKUDA
+                    Ponda Beat boy</em></p>
+            </div>
+        )
+    },
+    {
+        title: 'Mama Africa',
+        description: (
+            <div>
+                <p>[Chorus] : Black FW</p>
+                <p>My love for you
+                    Make me happy strong
+                    Your love for me
+                    Make me mature
+                    You are my feelings
+                    I go make you collabo
+                    You are my language
+                    I go love you forever</p>
+                <p>[Verse 1] : Del Claude & <strong>Black FW</strong></p>
+                <p> Sunlight in my day you help me to dream,
+                    For everything i'm today you hear my scream,
+                    Happy for your love, mature for you teaching
+                    Afraid of life but looking for your love,
+                    You showed me right from wrong
+                    Care of me and confort me
+                    Dans ton amour je suis né, you help to dream
+                    De tes mots tu m'as consolé you hear my scream
+                    Tendresse, caresse
+                    Transgression et punition
+                    Voilà que j'affrontre la vie,
+                    Les joies,les peines et les soucies alors let me t'adresser mille merci et pour moi tu
+                    restes une école de vie.</p>
+                <p><strong>True love you give to me
+                    And I swear I go love now
+                    We made it we made it
+                    Black FW One touch Images (Yaga)
+                    I can’t talk by your heart
+                    I can’t talk by your fever
+                    This be my language
+                    This be my success
+                    Mama I love you
+                    TBT 2014
+                    Mama you left us
+                    Amazing never talk
+                    For my success I need you
+                    For behavior I thank you
+                    You be my success my all my every thing(Everything)
+                    I take it back for my vibe
+                    I take it back for you Mama
+                    Every day you are cool with me
+                    Every day we are success
+                    Mama my language, my success, my beauty, my all my everything(My all my
+                    everything)</strong></p>
+                <p>[Chorus] : Black FW</p>
+                <p>[Interlude: Del Claude]
+                    For your example
+                    Your teaching
+                    You’ll be my everything
+                    I go love you forever
+                    You’ll be my number one </p>
+                <p>[Verse 2]: Fresh MC</p>
+                <p>Mama Africa plus forte que toutes
+                    Rien ne vaut plus cher que ton vrai amour
+                    T’es la source l’affection et l’éducation
+                    Tu donnes l’inspi’ à toute les nations
+                    Et grâce à toi aujourd’hui il y a l’évolution <strong>Grace à toi aujourd’hui il y a
+                        l’évolution</strong>
+                    . Quelle est ta place
+                    Quelle est ta classe
+                    T’es différente de ces putains des pétases
+                    T’as la beauté naturelle originale
+                    Et malgré ta souffrance tu restes vitale <strong>Tu restes vitale, tu restes vitale et
+                        malgré ta souffrance tu restes vitale</strong></p>
+                <p>[Chorus]: Black FW</p>
+                <p>[Outro]: Del Claude , Fresh MC & Black FW</p>
+                <p><em>Je parle de la femme
+                    Je parle de ta sœur
+                    Je parle de ta mère et toi tu fais semblant
+                    Je parle de la femme et toi tu te sens mal
+                    Je parle de ta sœur qui va perdre sa voix
+                    Je parle de la femme qui a souffert pour toi
+                    On parle de Mama Africa</em></p>
+            </div>
+        )
+    },
+    {
+        title: 'Black Mamba',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin condimentum sollicitudin eros, non sagittis sapien consequat vel. Praesent vel tortor ac mi placerat condimentum. Donec dictum posuere odio vel feugiat. Aenean sed sem tincidunt, rhoncus magna sit amet, convallis justo. Curabitur vitae imperdiet magna, non pharetra est. Proin eget auctor.'
+    },
+]
