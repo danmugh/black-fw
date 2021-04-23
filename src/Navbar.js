0e6a90dd29f496cdc6d95a4ccc7b6e4be93093b6
Dan Mugisho <danmmwx@gmail.com>
2021-04-23 16:05:44 +0300

    Introduces the navbar mobile

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react';
 import "./Navbar.css"
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import { menuData } from "./menuData";
-import { MenuFoldOutlined } from "@ant-design/icons";
+import { MenuFoldOutlined, CloseOutlined } from "@ant-design/icons";
 import Logo from "./assets/Police BLACK FW.png"
 import { Button } from 'antd';
 
 
 const Navbar = () => {
 
-    const [show, handleShow] = useState(false)
+    const [show, handleShow] = useState(false);
+    const [menu, setMenu] = useState(false);
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
@@ -22,9 +23,14 @@ const Navbar = () => {
         // }
     }, [])
 
+    const handleMenu = () => {
+        setMenu(!menu)
+    };
+
+
     return (
         <nav className={`nav ${show && 'nav__black'}`} >
-            <div className="logo" >
+            <div className="logo" style={{ display: `${menu ? 'none' : 'block' }`  }} >
                 <Router>
                     <Link to="#" className="navLink" >
                         {/*Logo*/}
@@ -33,11 +39,16 @@ const Navbar = () => {
                 </Router>
             </div>
 
-            <MenuFoldOutlined className="menu__bars" style={{ fontSize: '30px', color: 'white' }} />
+            <MenuFoldOutlined
+                className="menu__bars"
+                onClick={ handleMenu }
+                style={{ fontSize: '30px',
+                        color: 'white',
+                        display: `${menu ? 'none' : 'block' }` }} />
 
             {/*<div></div>*/}
 
-            <div className="navMenu">
+            <div className="navMenu" >
                 {menuData.map((item, index) => (
                     <Router>
                         <Link to={item.link} key={index} className="navLink" >
@@ -51,6 +62,24 @@ const Navbar = () => {
                 <Button
                     // shape="round"
                     className="navBtn__item" >Contact </Button>
+            </div>
+
+            <div>
+                <nav className={menu ? 'nav-menu active' : 'nav-menu'}>
+                    <ul className='nav-menu-items'  >
+                        <li className='navbar-toggle'>
+                            <a href="#">
+
+                                <CloseOutlined
+                                    onClick={handleMenu}
+                                    className='close-icon'
+                                    style={{ color: 'white', fontSize: '35px'}} />
+                            </a>
+                        </li>
+
+
+                    </ul>
+                </nav>
             </div>
 
         </nav>
